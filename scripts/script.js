2b9a3c486d6bcedd15fbc07804f7b9bf4e2ecc3f
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2022-12-11 23:55:53 -0500

    cloud sand moon added

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+// $('.cartoonContainer2').hide();
+
+// $('.cartoonContainer').on('click', function(){
+//     $(".cartoon").toggle();
+//     $('.cartoonContainer2').toggle();
+// });
+
+var soldierAttacking = document.getElementById('cartoon2');
+
+var soldierWalking = document.getElementById('cartoon');
+
+$(soldierAttacking).hide();
+
+
+}
